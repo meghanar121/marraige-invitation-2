55766de34cca67901135a57e5937d269092a3b26
meghanar121 <meghanar121@gmail.com>
2020-06-01 15:30:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+import D3 from './D3'
+class App extends Component {
+   render() {
+   return (
+      <div className='App'>
+      <div className='App-header'>
+      <h2>d3 dashboard</h2>
+      </div>
+      <div>
+      <D3 data={[5,9,1,3,9,8,5,3,1]} size={[500,500]} />
+      </div>
+      </div>
+   )
+   }
 }
-
-export default App;
+export default App
